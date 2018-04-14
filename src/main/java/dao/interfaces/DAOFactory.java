412d3717302ec6d1a05eaffba6224f76ad02a9d1
Serhii <sirko2097@gmail.com>
2018-04-14 23:44:42 +0300

    DAO interfaces were added.

---
+++ src/main/java/dao/interfaces/DAOFactory.java
@@ -0,0 +1,16 @@
+package dao.interfaces;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Data Access factory with methods
+ * for working with information.
+ * */
+public interface DAOFactory {
+    Connection getConnection() throws SQLException;
+
+    DAOCargo getDAOCargoImpl(Connection connection);
+
+    DAOUser getDAOUserImpl(Connection connection);
+}
