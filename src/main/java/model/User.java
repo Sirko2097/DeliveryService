3f453140fb165113964f8377aa02bdac4e4dd43a
Serhii <sirko2097@gmail.com>
2018-04-14 23:34:42 +0300

    Entities "Cargo" and "User" were added.

---
+++ src/main/java/model/User.java
@@ -0,0 +1,60 @@
+package model;
+
+import java.util.Objects;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String login;
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(firstName, user.firstName) &&
+                Objects.equals(lastName, user.lastName) &&
+                Objects.equals(login, user.login) &&
+                Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        
+        return Objects.hash(firstName, lastName, login, password);
+    }
+
+    public String getFirstName() {
+
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
